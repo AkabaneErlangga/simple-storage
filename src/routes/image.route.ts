a3579bad1b738de174aa34d2429c856d467e436c
choiruladamm <choiruladamm@gmail.com>
2024-05-03 10:26:15 +0700

    feat: add endpoint get all images

--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -1,11 +1,18 @@
-import { Router } from "express";
-import { deleteImage, getImage, getImagesByBucket, uploadImage } from "../controller/image.controller";
+import { Router } from 'express';
+import {
+  deleteImage,
+  getAllImages,
+  getImage,
+  getImagesByBucket,
+  uploadImage,
+} from '../controller/image.controller';
 
 const router = Router();
 
-router.post("/uploads", uploadImage);
-router.get("/:bucket", getImagesByBucket);
-router.get("/:bucket/:filename", getImage);
-router.delete("/:filename", deleteImage);
+router.get('/', getAllImages);
+router.post('/uploads', uploadImage);
+router.get('/:bucket', getImagesByBucket);
+router.get('/:bucket/:filename', getImage);
+router.delete('/:filename', deleteImage);
 
 export default router;
