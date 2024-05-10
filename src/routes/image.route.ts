b6efbe31cb54411b2594e3787c4143a81d126c9b
Er <ursawarrior157@gmail.com>
2024-05-10 10:55:26 +0700

    feat: trash system

--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
 	deleteImage,
+	destroyImage,
 	getAllImages,
 	getImage,
 	getImagesByBucket,
+	restoreImage,
 	uploadImage,
 } from "../controller/image.controller";
 
@@ -15,6 +17,8 @@ router.post("/uploads", uploadImage);
 router.get("/:bucket", getImagesByBucket);
 router.get("/:bucket/:filename", getImage);
 router.post("/uploads", uploadImage);
+router.put("/restore/:fileId", restoreImage);
+router.delete("/destroy/:fileId", destroyImage);
 router.delete("/:bucketId/:fileId", deleteImage);
 
 export default router;
