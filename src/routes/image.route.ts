b2c2f196973a37140acc1261c3628bc93f0b73f3
Er <ursawarrior157@gmail.com>
2024-05-28 10:10:25 +0700

    feat: auth imiddleware in image route

--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -9,18 +9,18 @@ import {
 	restoreImage,
 	uploadImage,
 } from "../controller/image.controller";
+import authMiddleware from "../middlewares/auth.middleware";
 
 const router = Router();
 
-router.get("/", getAllImages);
-router.get("/deleted", getDeletedImages);
-router.get("/:bucketName", getImagesByBucket);
+router.get("/", authMiddleware, getAllImages);
+router.get("/deleted", authMiddleware, getDeletedImages);
+router.get("/:bucketName", authMiddleware, getImagesByBucket);
 router.post("/uploads", uploadImage);
-router.get("/:bucket", getImagesByBucket);
+router.get("/:bucket", authMiddleware, getImagesByBucket);
 router.get("/:bucket/:filename", getImage);
-router.post("/uploads", uploadImage);
-router.put("/restore/:fileId", restoreImage);
-router.delete("/destroy/:fileId", destroyImage);
-router.delete("/:bucketId/:fileId", deleteImage);
+router.put("/restore/:fileId", authMiddleware, restoreImage);
+router.delete("/destroy/:fileId", authMiddleware, destroyImage);
+router.delete("/:bucketId/:fileId", authMiddleware, deleteImage);
 
 export default router;
